e7282633782bdcf58820302178402296e8eba9aa
MayderC <maydercortes1@gmail.com>
2022-03-01 22:43:37 -0600

    add, options in sequelize conection

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,12 @@
-const { Sequelize } = require('sequelize');
-const ENV = require('../config/environments')
+const { Sequelize } = require("sequelize");
+const ENV = require("../config/environments");
 
-
-const sequelize = new Sequelize(ENV.DB_STRING_CONECTION) 
-module.exports = {sequelize}
+const sequelize = new Sequelize(ENV.DB_STRING_CONECTION, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+module.exports = { sequelize };
