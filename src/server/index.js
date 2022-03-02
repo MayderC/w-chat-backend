182ce31e968e10363401449b85df19cb1d6b0428
MayderC <maydercortes1@gmail.com>
2022-03-01 23:19:18 -0600

    sync force

--- src/server/index.js
+++ src/server/index.js
@@ -29,7 +29,7 @@ class Server {
   }
   async conexion() {
     await sequelize.authenticate();
-    //await sequelize.sync({ force: true });
+    await sequelize.sync({ force: true });
     console.log("Postgres ON");
   }
   middlewares() {
