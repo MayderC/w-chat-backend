c600c11ecb934d096faa5ae03b73ffb12549fd47
Mayder C <44930667+MayderC@users.noreply.github.com>
2023-03-14 15:07:40 -0600

    feat: add express validatior

--- src/Infraestructure/webserver/controllers/auth/auth.controller.ts
+++ src/Infraestructure/webserver/controllers/auth/auth.controller.ts
@@ -10,24 +10,19 @@ export class AuthController {
   }
 
   async userVerify(req: Request, res: Response) {
-    const payload = decodeToken(req.headers["token"] || "");
-    if (payload.id === "") return res.status(401).send({ msg: "Error" });
-
+    const payload = decodeToken(req.headers["token"]);
     const data = await this._authService.getProfile(payload.id);
     return data ? res.send(data) : res.send({ msg: "Error" });
   }
 
   async userRegister(req: Request, res: Response) {
-    const { username, password } = req.body;
-    if (!username || !password) return res.status(400).send({ msg: "Error" });
-
     try {
+      const { username, password } = req.body;
       const user = await this._authService.register(username, password);
       if (!user) return res.send({ msg: "Error" });
 
       const token = await createToken({ id: user.id });
-      const data = { user, token };
-      return res.send({ data });
+      return res.send({ data: { user, token } });
     } catch (error) {
       return res.send({ msg: "Error" });
     }
@@ -35,8 +30,6 @@ export class AuthController {
 
   async userLogin(req: Request, res: Response) {
     const { username, password } = req.body;
-    if (!username || !password) return res.send({ msg: "Error" });
-
     try {
       const user = await this._authService.login(username, password);
       if (!user) return res.send({ msg: "Error" });
--- src/Infraestructure/webserver/routes/auth.routes.ts
+++ src/Infraestructure/webserver/routes/auth.routes.ts
@@ -1,5 +1,7 @@
 import { AuthController } from "../controllers/auth/auth.controller";
 import { Router } from "express";
+import { validations } from "../middlewares/valitadionResults";
+import { check, header } from "express-validator";
 
 export class AuthRoutes {
   private _router;
@@ -8,10 +10,39 @@ export class AuthRoutes {
     this._router = Router();
     this._router.post(
       "/register",
+      this.userRegisterMiddle,
       authController.userRegister.bind(authController)
     );
-    this._router.post("/login", authController.userLogin.bind(authController));
-    this._router.get("/verify", authController.userVerify.bind(authController));
+    this._router.post(
+      "/login",
+      this.userLoginMiddle,
+      authController.userLogin.bind(authController)
+    );
+    this._router.get(
+      "/verify",
+      this.userVerifyMiddle,
+      authController.userVerify.bind(authController)
+    );
+  }
+
+  get userLoginMiddle() {
+    return [...this.commonMiddle];
+  }
+
+  get commonMiddle() {
+    return [
+      check("username", "the username must not be empty").notEmpty(),
+      check("password", "the password must not be empty").notEmpty(),
+      validations,
+    ];
+  }
+
+  get userRegisterMiddle() {
+    return [...this.commonMiddle];
+  }
+
+  get userVerifyMiddle() {
+    return [header("token", "the  token is required").notEmpty()];
   }
 
   get router() {
